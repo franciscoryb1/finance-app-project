cd65308108a25ee2d5fb53ce1dfae9ddc00c0fb4
franciscoryb1 <desarrollo2@renovasrl.com.ar>
2025-05-12 15:43:44 -0300

    ADD ensureUser.ts para verificar si el usuario esta en la bbdd de postgres

---
+++ src/lib/ensureUser.ts
@@ -0,0 +1,28 @@
+import { auth0 } from "./auth0";
+import { prisma } from "./prisma";
+
+export async function ensureUserExists() {
+  const session = await auth0.getSession();
+  const user = session?.user;
+
+  if (!user) return null;
+
+  let dbUser = await prisma.user.findUnique({
+    where: { auth_id: user.sub },
+  });
+
+  if (!dbUser) {
+    dbUser = await prisma.user.create({
+      data: {
+        auth_id: user.sub,
+        email: user.email || "",
+        name: user.name || "",
+        currency: "ARS", // Default inicial
+      },
+    });
+  }
+
+  console.log(dbUser);
+
+  return dbUser;
+}
