98fe8e4f5877188bf6b663a4e84290d294a4a1ed
franciscoryb1 <franciscoryb1@hotmail.com>
2025-05-12 21:30:50 -0300

    FIX TransactionModal.tsx, agregue onClose funciton

--- src/components/TransactionModal.tsx
+++ src/components/TransactionModal.tsx
@@ -1,19 +1,16 @@
-import { DialogContent, DialogHeader, DialogTitle, DialogFooter } from "@/components/ui/dialog"
-import { Button } from "@/components/ui/button"
+"use client"
+
+import { DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
 import TransactionForm from "./TransactionForm"
 
-export default function TransactionModal() {
+export default function TransactionModal({ onClose }: { onClose: () => void }) {
   return (
     <DialogContent className="sm:max-w-[500px]">
       <DialogHeader>
         <DialogTitle>Nueva transacción</DialogTitle>
       </DialogHeader>
 
-      <TransactionForm />
-
-      <DialogFooter>
-        <Button type="submit" form="transaction-form">Guardar</Button>
-      </DialogFooter>
+      <TransactionForm onSuccess={onClose} />
     </DialogContent>
   )
 }
