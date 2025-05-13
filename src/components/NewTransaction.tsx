7351854d1381a78ecdb2baedd59e97c4bdd89f15
franciscoryb1 <franciscoryb1@hotmail.com>
2025-05-12 21:30:10 -0300

    FIX NewTransaction, agregue state de open con useState para el modal

--- src/components/NewTransaction.tsx
+++ src/components/NewTransaction.tsx
@@ -1,17 +1,21 @@
 'use client'
 
+import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import { Dialog, DialogTrigger } from "@/components/ui/dialog"
 import TransactionModal from "./TransactionModal"
 
 export default function NewTransaction() {
+  const [open, setOpen] = useState(false)
+
   return (
     <div className="bg-gray-100 p-4 rounded-xl shadow">
-      <Dialog>
+      <Dialog open={open} onOpenChange={setOpen}>
         <DialogTrigger asChild>
           <Button variant="default">Nuevo gasto / ingreso</Button>
         </DialogTrigger>
-        <TransactionModal />
+
+        <TransactionModal onClose={() => setOpen(false)} />
       </Dialog>
     </div>
   )
