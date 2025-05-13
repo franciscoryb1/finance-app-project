3f14fe5ffe41416df3931af1fca362fb9c2380fe
franciscoryb1 <franciscoryb1@hotmail.com>
2025-05-12 21:28:52 -0300

    ADD /api/categories route.ts

---
+++ src/app/api/categories/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server"
+import { prisma } from "@/lib/prisma"
+import { ensureUserExists } from "@/lib/ensureUser"
+
+export async function GET(request: NextRequest) {
+  const user = await ensureUserExists()
+  if (!user) return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
+
+
+
+  const categories = await prisma.category.findMany({
+    where: {
+      user_id: user.id,
+    },
+    select: {
+      id: true,
+      name: true,
+    },
+    orderBy: {
+      name: "asc",
+    },
+  })
+
+  console.log("categories", categories);
+  console.log("user", user);
+
+  return NextResponse.json(categories)
+}
