4daa3b7771bae3c5ce1caa1d0cd2bd5fbbb9c3e1
franciscoryb1 <franciscoryb1@hotmail.com>
2025-05-15 10:56:21 -0300

    ADD Sonner en layout.tsx

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -6,6 +6,7 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarInset } from "@/components/ui/sidebar";
 import { SiteHeader } from "@/components/site-header";
+import { Toaster } from "@/components/ui/sonner";
 
 export default async function RootLayout({
   children,
@@ -24,16 +25,23 @@ export default async function RootLayout({
   // Acá ya tenés acceso al `dbUser.id`, que es tu ID real en la base de datos
 
   return (
-    <SidebarProvider>
-      <AppSidebar variant="inset" />
-      <SidebarInset>
-        <SiteHeader />
-        <div className="flex flex-1 flex-col">
-          <div className="@container/main flex flex-1 flex-col gap-2">
-            {children}
+    <>
+      <Toaster />
+      {/* el resto del layout */}
+      <SidebarProvider>
+        <AppSidebar variant="inset" />
+        <SidebarInset>
+          <SiteHeader />
+          <div className="flex flex-1 flex-col">
+            <div className="@container/main flex flex-1 flex-col gap-2">
+              {children}
+            </div>
           </div>
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
+        </SidebarInset>
+      </SidebarProvider>
+    </>
+
+
+
   );
 }
